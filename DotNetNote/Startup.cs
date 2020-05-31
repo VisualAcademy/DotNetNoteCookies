ed765335b6f7910995e9312de683d1afac746a34
RedPlus <redplus@redplus.net>
2020-05-31 09:55:10 +0900

    ASP.NET Core 3.1에서 쿠키 인증으로 회원가입, 로그인, 로그아웃, 회원 정보 표시 기능 구현 완료

--- DotNetNote/Startup.cs
+++ DotNetNote/Startup.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DotNetNote.Common;
+using DotNetNote.Components;
+using DotNetNote.Models;
+using DotNetNote.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +23,43 @@ namespace DotNetNote
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            ////[!] Configuration: JSON 파일의 데이터를 POCO 클래스에 주입
+            //services.Configure<DotNetNoteSettings>(Configuration.GetSection("DotNetNoteSettings"));
+
+            services.AddAuthentication("Cookies")
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/User/Login/";
+                    options.AccessDeniedPath = "/User/Forbidden/";
+                });
+
+            //[DI] 의존성 주입(Dependency Injection)
+            DependencyInjectionContainer(services);
+        }
+
+        /// <summary>
+        /// 의존성 주입 관련 코드만 따로 모아서 관리
+        /// - 리포지토리 등록
+        /// </summary>
+        private void DependencyInjectionContainer(IServiceCollection services)
+        {
+            //[?] ConfigureServices가 호출되기 전에는 DI(종속성 주입)가 설정되지 않습니다.
+
+            //[DNN][!] Configuration 개체 주입: 
+            //    IConfiguration 또는 IConfigurationRoot에 Configuration 개체 전달
+            //    appsettings.json 파일의 데이터베이스 연결 문자열을 
+            //    리포지토리 클래스에서 사용할 수 있도록 설정
+            // IConfiguration 주입 -> Configuration의 인스턴스를 실행 
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            //[User][5] 회원 관리
+            services.AddTransient<IUserRepository, UserRepository>();
+            // LoginFailedManager
+            services.AddTransient<ILoginFailedRepository, LoginFailedRepository>();
+            services.AddTransient<ILoginFailedManager, LoginFailedManager>();
+            // 사용자 정보 보기 전용 컴포넌트
+            services.AddTransient<IUserModelRepository, UserModelRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +80,7 @@ namespace DotNetNote
 
             app.UseRouting();
 
+            app.UseAuthentication(); 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
